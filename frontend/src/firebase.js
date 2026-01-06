af2cfb7f3042bab27225ae4b61e4581934624f9a
Raji2k6 <rajivijayakumar06@gmail.com>
2026-01-06 16:52:49 +0530

    Removed Secrets

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,21 +1,3 @@
-// Import the functions you need from the SDKs you need
-/*import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAvSOIXInRcvrdOPHnyJcovPibZvT0fFms",
-  authDomain: "cloud-social-media-app-731bd.firebaseapp.com",
-  projectId: "cloud-social-media-app-731bd",
-  storageBucket: "cloud-social-media-app-731bd.firebasestorage.app",
-  messagingSenderId: "540782058686",
-  appId: "1:540782058686:web:bb2eb78a802ca472ba12ef"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);*/
-// Import Firebase SDK
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
